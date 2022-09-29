c1ef22050ab54f3d1cf2020e5392e3f99791d6af
Aryan Jagadale <79126635+Aryan-Jagadale@users.noreply.github.com>
2022-09-29 19:31:13 +0530

    Update Array.js
    Map,reduce && filter done

--- Array.js
+++ Array.js
@@ -35,6 +35,13 @@ console.log(isAdmin)*/
 //false
 
 
+
+
+
+
+
+
+
 /*find*/
 const users = [
   {
@@ -73,3 +80,95 @@ console.log(names)
 console.log(languages)
 
 /*Map function*/
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+
+/*Filter function*/
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+
+/*Reduce function */
+const arr = [5,1,3,2,6]
+
+//Sum
+function findSum(arr){
+  let sum = 0
+  for(let i= 0; i < arr.length; i++){
+    sum += arr[i] 
+  }
+  return sum;
+}
+
+//Here acc === sum &&& curr === arr[i]
+const output = arr.reduce(function(acc,curr) {
+  acc = acc + curr
+  return acc
+},0)
+
+//console.log(output)
+
+//Max
+function findMax(arr){
+  let max = 0
+  for(let i= 0; i < arr.length; i++){
+     if(arr[i] > max) {
+       max = arr[i]
+     }
+  }
+  return max;
+}
+//console.log(findMax(arr))
+const result = arr.reduce(function(acc,curr){
+   if(curr>acc){
+  	acc = curr
+	}
+	return acc
+},0)
+console.log(result)
+
+/*Tricky reduce*/
+const users = [
+  {firstname:"akshay",lastname:"saini",age:26},
+    {firstname:"donald",lastname:"trump",age:75},
+{firstname:"elon",lastname:"musk",age:50},
+  {firstname:"a",lastname:"j",age:26},
+]
+
+// {26:2,75:1,50:1}
+
+const ageList = users.reduce(function(acc,user){
+  if(acc[user.age]){
+    acc[user.age] = ++acc[user.age]
+  }else {
+    acc[user.age] = 1
+  }
+  return acc
+},{})
+console.log(ageList)
+
+
+/*Chaining */
+const users = [
+  {firstname:"akshay",lastname:"saini",age:26},
+    {firstname:"donald",lastname:"trump",age:75},
+{firstname:"elon",lastname:"musk",age:50},
+  {firstname:"a",lastname:"j",age:26},
+]
+
+// {26:2,75:1,50:1}
+
+const nameList = users.filter(user => user.age < 30)
+const ageList = nameList.map(obj => obj.firstname )
+console.log(ageList)
+
+const output = users.filter(x => x.age<30).map(x => x.firstname)
+console.log(output)
+
+
+
+
+
+
+
+
+
+
+
